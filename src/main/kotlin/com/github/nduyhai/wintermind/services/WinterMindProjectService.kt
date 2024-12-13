2392ff4ef61e79a9f9bd7696152b32d06712be38
hai.nguyen <15892244+nduyhai@users.noreply.github.com>
2024-12-13 10:55:10 +0700

    feat: clean code

--- src/main/kotlin/com/github/nduyhai/wintermind/services/WinterMindProjectService.kt
+++ src/main/kotlin/com/github/nduyhai/wintermind/services/WinterMindProjectService.kt
@@ -3,13 +3,13 @@ package com.github.nduyhai.wintermind.services
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import com.github.nduyhai.wintermind.MyBundle
+import com.github.nduyhai.wintermind.WinterMindBundle
 
 @Service(Service.Level.PROJECT)
-class MyProjectService(project: Project) {
+class WinterMindProjectService(project: Project) {
 
     init {
-        thisLogger().info(MyBundle.message("projectService", project.name))
+        thisLogger().info(WinterMindBundle.message("projectService", project.name))
         thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
     }
 
--- src/main/kotlin/com/github/nduyhai/wintermind/toolWindow/WinterMindWindowFactory.kt
+++ src/main/kotlin/com/github/nduyhai/wintermind/toolWindow/WinterMindWindowFactory.kt
@@ -8,12 +8,12 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import com.github.nduyhai.wintermind.MyBundle
-import com.github.nduyhai.wintermind.services.MyProjectService
+import com.github.nduyhai.wintermind.WinterMindBundle
+import com.github.nduyhai.wintermind.services.WinterMindProjectService
 import javax.swing.JButton
 
 
-class MyToolWindowFactory : ToolWindowFactory {
+class WinterMindWindowFactory : ToolWindowFactory {
 
     init {
         thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
@@ -29,15 +29,15 @@ class MyToolWindowFactory : ToolWindowFactory {
 
     class MyToolWindow(toolWindow: ToolWindow) {
 
-        private val service = toolWindow.project.service<MyProjectService>()
+        private val service = toolWindow.project.service<WinterMindProjectService>()
 
         fun getContent() = JBPanel<JBPanel<*>>().apply {
-            val label = JBLabel(MyBundle.message("randomLabel", "?"))
+            val label = JBLabel(WinterMindBundle.message("randomLabel", "?"))
 
             add(label)
-            add(JButton(MyBundle.message("shuffle")).apply {
+            add(JButton(WinterMindBundle.message("shuffle")).apply {
                 addActionListener {
-                    label.text = MyBundle.message("randomLabel", service.getRandomNumber())
+                    label.text = WinterMindBundle.message("randomLabel", service.getRandomNumber())
                 }
             })
         }
